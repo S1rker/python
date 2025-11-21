8c12ac5e0694709a85535add468485540cfc1900
S1rker <carter.a.moon@gmail.com>
2025-11-21 17:47:21 -0600

    finshed tele

--- television.py
+++ television.py
@@ -1,3 +1,69 @@
 class Television():
+    '''
+    
+    
+    '''
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+
     def __init__(self):
-        pass
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+
+
+    def power(self):
+        if self.__status == False:
+            self.__status = True
+        else:
+            self.__status = False
+
+    def mute(self):
+        # Only allow mute toggle when TV is powered on
+        if self.__status:
+            self.__muted = not self.__muted
+
+    def channel_up(self):
+        if self.__status == True:
+            if self.__channel == self.MAX_CHANNEL:
+                self.__channel = self.MIN_CHANNEL
+            else:
+                self.__channel += 1
+
+    def channel_down(self):
+        if self.__status == True:
+            if self.__channel == self.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
+            else:
+                self.__channel -= 1
+
+    def volume_up(self):
+        if self.__status == True:
+            # Changing the volume should unmute the TV
+            if self.__muted:
+                self.__muted = False
+            if self.__volume < self.MAX_VOLUME:
+                self.__volume += 1
+
+    def volume_down(self):
+        if self.__status == True:
+            # Changing the volume should unmute the TV
+            if self.__muted:
+                self.__muted = False
+            if self.__volume > self.MIN_VOLUME:
+                self.__volume -= 1
+
+
+    def __str__(self):
+        # When muted (and powered on) report volume as 0 for display only
+        display_volume = 0 
+        if self.__status and self.__muted:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {display_volume}'
+        else:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
+        
+    
